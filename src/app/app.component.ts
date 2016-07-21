d4c6c27504767b3b1ff91c55b25e072574c95ded
Listbd <listbd@gmail.com>
2016-07-21 08:51:35 -0700

    using observable and async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { AboutComponent} from './about/about.component';
 import { CustomersComponent} from './customer/customers.component';
 
+//import { HTTP_PROVIDERS } from '@angular/http';
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [AboutComponent, CustomersComponent]
+  directives: [AboutComponent, CustomersComponent],
+//  providers: [HTTP_PROVIDERS]
 })
 export class AppComponent {
 // [ ] - property binding
--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core'
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx'; // get everything from Rx
+// import 'rxjs/add/operators/map'; // just get map
+
+const URL_CUSTOMER = 'app/customers.json';
 
 @Injectable()
 export class CustomerService  {
+    constructor(private _http: Http) {}
+
     getCustomers() {  
-        return [
-            { id: 1, name: 'Alpha'},
-            { id: 2, name: 'Bravo'},
-            { id: 3, name: 'Charlie'}
-        ];        
+        return this._http.get(URL_CUSTOMER)
+            .map((response: Response) => response.json());        
     }
-    constructor() {}
 }
